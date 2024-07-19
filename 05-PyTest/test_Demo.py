bf60bbb22c3bfd208fc93f40758c1cb40d3c4c0d
prashantbohra01 <prashantb4157@gmail.com>
2024-07-19 16:55:25 +0530

    Pytest Framework

---
+++ 05-PyTest/test_Demo.py
@@ -0,0 +1,12 @@
+# Pytest file name should start with "test_" or end with "_test"
+
+# Pytest methods/function name should always start with "test"
+
+# Any code should be wrapped in methods
+
+def testFirstProgram():
+    print("Hello-World")
+
+
+def testGreetAll():
+    print("Good Morning")
---
+++ 05-PyTest/test_Demo2.py
@@ -0,0 +1,4 @@
+def testPrograms():
+    msg = "Hello"
+
+    assert msg == "Hi", "Test failed because strings did not matched"

6f99d1a425bdaa2a0948d0c607b7b99e1c7b8e95
prashantbohra01 <prashantb4157@gmail.com>
2024-07-30 13:23:11 +0530

    Html reports

--- 05-PyTest/test_parameterizing.py
+++ 05-PyTest/test_parameterizing.py
@@ -1,5 +1,8 @@
 # Parameterizing test with multiple data sets using fixtures
 
+#data driven and parameterization can be done with return statement in tuple format
+# when you define fixture scope to class only, it will run once before class is initiated and at the end
+
 import pytest
 
 def test_crossBrowser(crossBrowser):
